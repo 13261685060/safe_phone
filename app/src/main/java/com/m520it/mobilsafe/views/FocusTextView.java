7786eb2180db711c8a24c594e07e296a5c448fb7
13261685060 <13261685060@163.com>
2019-03-15 22:48:19 +0800

    first day vedio

---
+++ app/src/main/java/com/m520it/mobilsafe/views/FocusTextView.java
@@ -0,0 +1,11 @@
+package com.m520it.mobilsafe.views;
+
+/**
+ * @author Administrator
+ * @version $Rev$
+ * @des ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class FocusTextView {
+}

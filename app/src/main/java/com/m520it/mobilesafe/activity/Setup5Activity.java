f926efd9d9f3b30100750379114791255b4c1353
13261685060 <13261685060@163.com>
2019-03-20 21:57:32 +0800

    添加完手机防盗的五个界面，未设置点击事件

---
+++ app/src/main/java/com/m520it/mobilesafe/activity/Setup5Activity.java
@@ -0,0 +1,25 @@
+package com.m520it.mobilesafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.m520it.mobilesafe.R;
+
+/**
+ * @author Administrator
+ * @version $Rev$
+ * @des ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class Setup5Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceStata){
+        super.onCreate(savedInstanceStata);
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_setup4);
+    }
+}

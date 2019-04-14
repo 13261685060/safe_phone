bd3aa783af4d85d0996153270f6f8a60615c5cca
13261685060 <13261685060@163.com>
2019-04-14 17:42:08 +0800

    设置完软件管家里软件卸载等功能

--- app/src/main/java/com/m520it/mobilsafe/activity/AppManagerActivity.java
+++ app/src/main/java/com/m520it/mobilsafe/activity/AppManagerActivity.java
@@ -2,7 +2,13 @@ package com.m520it.mobilsafe.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -24,6 +30,7 @@ import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.m520it.mobilsafe.R;
 import com.m520it.mobilsafe.bean.AppInfo;
@@ -40,7 +47,7 @@ import java.util.List;
  * @updateAuthor $Author$
  * @updateDes ${TODO}
  */
-class AppManagerActivity extends Activity {
+class AppManagerActivity extends Activity implements View.OnClickListener {
     private TextView tv_rom;
     private TextView tv_sd;
     private ListView lv_appmanager;
@@ -61,6 +68,7 @@ class AppManagerActivity extends Activity {
     private AppManagerAdapter adapter;
     private AppInfo appInfo;
     private PopupWindow popupWindow;
+    private InnerReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,8 +80,13 @@ class AppManagerActivity extends Activity {
     }
 
 
-
     private void initView() {
+        receiver = new InnerReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        //卸载APP特有
+        intentFilter.addDataScheme("package");
+        registerReceiver(receiver, intentFilter);
         setContentView(R.layout.activity_appmanager);
         tv_rom = (TextView) findViewById(R.id.tv_rom);
         tv_sd = (TextView) findViewById(R.id.tv_sd);
@@ -100,11 +113,10 @@ class AppManagerActivity extends Activity {
                 userInfos = new ArrayList<AppInfo>();
                 systemInfos = new ArrayList<AppInfo>();
                 infos = SystemAppInfo.getAllAppInfo(AppManagerActivity.this);
-                for(AppInfo info:infos)
-                {
-                    if (info.isUser()){
+                for (AppInfo info : infos) {
+                    if (info.isUser()) {
                         userInfos.add(info);
-                    }else{
+                    } else {
                         systemInfos.add(info);
                     }
                 }
@@ -123,15 +135,16 @@ class AppManagerActivity extends Activity {
             public void onScrollStateChanged(AbsListView view, int scrollState) {
 
             }
+
             //动作监听
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
                 dismissPP();
-                if(userInfos!=null && systemInfos!=null){
-                    if (firstVisibleItem>userInfos.size()){
-                        tv_name_count.setText("系统软件"+systemInfos.size()+"个");
-                    }else{
-                        tv_name_count.setText("用户软件"+userInfos.size()+"个");
+                if (userInfos != null && systemInfos != null) {
+                    if (firstVisibleItem > userInfos.size()) {
+                        tv_name_count.setText("系统软件" + systemInfos.size() + "个");
+                    } else {
+                        tv_name_count.setText("用户软件" + userInfos.size() + "个");
                     }
                 }
             }
@@ -140,23 +153,23 @@ class AppManagerActivity extends Activity {
         lv_appmanager.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                if (position == 0){ 
+                if (position == 0) {
                     return;
-                }else if(position == userInfos.size()+1){
-                return;
-                }else if(position<=userInfos.size()){
+                } else if (position == userInfos.size() + 1) {
+                    return;
+                } else if (position <= userInfos.size()) {
                     appInfo = userInfos.get(position - 1);
-                }else{
-                    appInfo = systemInfos.get(position - userInfos.size()-2);
+                } else {
+                    appInfo = systemInfos.get(position - userInfos.size() - 2);
                 }
                 dismissPP();
-                View ppview = View.inflate(getApplicationContext(),R.layout.view_pp,null);
-                popupWindow = new PopupWindow(ppview,-2,-2);
+                View ppview = View.inflate(getApplicationContext(), R.layout.view_pp, null);
+                popupWindow = new PopupWindow(ppview, -2, -2);
                 //如果pp要显示动画，必须设置背景
-                popupWindow.setBackgroundDrawable(new.ColorDrawable());
+                popupWindow.setBackgroundDrawable(new ColorDrawable());
                 int[] location = new int[2];
                 view.getLocationInWindow(location);
-                popupWindow.showAtLocation(adapterView, Gravity.LEFT+Gravity.TOP,60,location[1]);
+                popupWindow.showAtLocation(adapterView, Gravity.LEFT + Gravity.TOP, 60, location[1]);
                 //透明度渐变动画
                 AlphaAnimation aa = new AlphaAnimation(0.1f, 1.0f);
                 aa.setDuration(300);
@@ -168,9 +181,14 @@ class AppManagerActivity extends Activity {
                 set.addAnimation(aa);
                 set.addAnimation(sa);
                 ppview.startAnimation(set);
+                ppview.findViewById(R.id.ll_uninstall).setOnClickListener(AppManagerActivity.this);
+                ppview.findViewById(R.id.ll_share).setOnClickListener(AppManagerActivity.this);
+                ppview.findViewById(R.id.ll_start).setOnClickListener(AppManagerActivity.this);
+                ppview.findViewById(R.id.ll_message).setOnClickListener(AppManagerActivity.this);
             }
         });
     }
+
     private void dismissPP() {
         if (popupWindow != null) {
             popupWindow.dismiss();
@@ -178,12 +196,74 @@ class AppManagerActivity extends Activity {
         }
 
     }
+
+   //点击事件
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.ll_uninstall:
+                uninstallApp();
+                break;
+            case R.id.ll_start:
+                startApp();
+                break;
+            case R.id.ll_share:
+                shareApp();
+                break;
+            case R.id.ll_message:
+                messageApp();
+                break;
+
+        }
+
+    }
+
+    private void messageApp() {
+        Intent intent=new Intent();
+        intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.setData(Uri.parse("package:"+appInfo.getPackname()));
+        startActivity(intent);
+    }
+
+    private void shareApp() {
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.SEND");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"这个是我很好用的一款手机卫士,如果你喜欢,拿U盘来考"+appInfo.getPackname());
+        startActivity(intent);
+
+    }
+
+    private void startApp() {
+        Intent launchIntentForPackage = this.getPackageManager().getLaunchIntentForPackage(appInfo.getPackname());
+        startActivity(launchIntentForPackage);
+
+
+
+    }
+
+    private void uninstallApp() {
+        if (appInfo.isUser()){
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            intent.setAction("android.intent.action.DELETE");
+            intent.addCategory("android.intent.category.DEFAULT");
+            intent.setData(Uri.parse("paclage:"+appInfo.getPackname()));
+            startActivity(intent);
+        }else{
+            Toast.makeText(AppManagerActivity.this,"系统软件不能卸载",Toast.LENGTH_SHORT).show();
+        }
+    }
+
     class AppManagerAdapter extends BaseAdapter {
         private AppInfo appInfo;
 
         @Override
         public int getCount() {
-            return userInfos.size()+systemInfos.size()+2;
+            return userInfos.size() + systemInfos.size() + 2;
         }
 
         @Override
@@ -213,51 +293,62 @@ class AppManagerActivity extends Activity {
                 tv.setTextSize(18);
                 return tv;
             } else if
-                (position <= userInfos.size()){
+            (position <= userInfos.size()) {
                 position = position - 1;
                 appInfo = userInfos.get(position);
-            }else{
-                position = position-userInfos.size()-2 ;
-            appInfo= systemInfos.get(position);
+            } else {
+                position = position - userInfos.size() - 2;
+                appInfo = systemInfos.get(position);
             }
 
 
             View view = null;
             ViewHolder holder;
-            if (convertView!= null && convertView instanceof RelativeLayout){
+            if (convertView != null && convertView instanceof RelativeLayout) {
                 view = convertView;
                 holder = (ViewHolder) view.getTag();
 
 
-            }else{
+            } else {
 
                 holder = new ViewHolder();
-                view = View.inflate(getApplicationContext(),R.layout.item_appmanager_view,null);
+                view = View.inflate(getApplicationContext(), R.layout.item_appmanager_view, null);
                 holder.iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
-                holder.tv_name= (TextView) view.findViewById(R.id.tv_name);
+                holder.tv_name = (TextView) view.findViewById(R.id.tv_name);
                 holder.tv_position = (TextView) view.findViewById(R.id.tv_position);
                 holder.tv_size = (TextView) view.findViewById(R.id.tv_size);
                 view.setTag(holder);
             }
             holder.iv_icon.setImageDrawable(appInfo.getIcon());
-            holder.tv_size.setText(Formatter.formatFileSize(getApplicationContext(),appInfo.getSize())+"");
+            holder.tv_size.setText(Formatter.formatFileSize(getApplicationContext(), appInfo.getSize()) + "");
             holder.tv_name.setText(appInfo.getName());
-            holder.tv_position.setText(appInfo.isRom()?"手机内存":"SD卡");
+            holder.tv_position.setText(appInfo.isRom() ? "手机内存" : "SD卡");
             return view;
         }
     }
 
-    class ViewHolder{
+    class ViewHolder {
         ImageView iv_icon;
         TextView tv_name;
         TextView tv_size;
         TextView tv_position;
     }
+
     @Override
-    protected void onDestory(){
+    protected void onDestroy() {
         dismissPP();
+        unregisterReceiver(receiver);
         super.onDestroy();
     }
 
-
+    class InnerReceiver extends BroadcastReceiver{
+        //一旦有软件被卸载，这个方法就会被调用
+        @Override
+        public void onReceive(Context context,Intent intent){
+        if(appInfo.isUser()){
+                userInfos.remove(appInfo);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
 }
